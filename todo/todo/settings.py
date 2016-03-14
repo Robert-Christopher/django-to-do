ed690f0a35532c5883d6989efa1fb033361f7a2d
Robert Christopher <antonyrobertchristopher@gmail.com>
2016-03-14 14:17:41 +0530

    using postgreSQL db

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -72,14 +72,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'todo.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'todo',
     }
 }
 
