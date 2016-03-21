b9008be9a87c73ed8014f2ddefe268f8b5c0137b
Robert Christopher <antonyrobertchristopher@gmail.com>
2016-03-21 23:06:39 +0530

    inline formset check

--- todo/tasks/forms.py
+++ todo/tasks/forms.py
@@ -1,19 +1,32 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, inlineformset_factory
 from django import forms
-from .models import Task
+from .models import Task, Tag, Tagmap
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Fieldset, ButtonHolder, Submit
 
 class TaskForm(ModelForm):
     class Meta:
         model = Task
-        fields = ['name', 'description', 'tags']
+        fields = ['name', 'description']
 
     name = forms.CharField(max_length=100)
     description = forms.CharField()
-    tags = forms.CharField()
     
     def __init__(self, *args, **kwargs):
         self.helper = FormHelper()
         self.helper.add_input(Submit('submit', 'Save'))
         super(TaskForm, self).__init__(*args, **kwargs)
+        
+class TagForm(ModelForm):
+    class Meta:
+        model = Tag
+        fields = ['name']
+ 
+    name = forms.CharField(max_length=100, widget=forms.Textarea())
+    
+    def __init__(self, *args, **kwargs):
+        self.helper = FormHelper()
+        self.helper.add_input(Submit('submit', 'Save'))
+        super(TagForm, self).__init__(*args, **kwargs)
+
+TaskFormSet = inlineformset_factory(Task, Task.tags.through, fields='__all__')
